73a6318439ffd272da057a0a9c2f540699615082
Jessusys <129582224+Jessusys@users.noreply.github.com>
2023-04-09 14:20:33 +0700

    Update mashaLOX-HTML5-Shipping.data.js

--- mashaLOX-HTML5-Shipping.data.js
+++ mashaLOX-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/User/Documents/Unreal Projects/mashaLOX/Binaries/HTML5/mashaLOX-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'mashaLOX-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/1zyamzy1hczwiip/mashaLOX-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/1zyamzy1hczwiip/mashaLOX-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
